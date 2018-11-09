a77f7867e140ad966dd9f05f22a348b7f0bae4d3
EclairCat <rabie1997@gmail.com>
2018-11-09 13:44:49 +0100

    Ajout des dossiers de depart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitListComponent } from './produit-list/produit-list.component';
+import { EspaceClientComponent } from './espace-client/espace-client.component';
+import { MentionsLegalComponent } from './mentions-legal/mentions-legal.component';
+import { PolitiqueConfComponent } from './politique-conf/politique-conf.component';
+import { ContactComponent } from './contact/contact.component';
+import { SocieteComponent } from './societe/societe.component';
+import { FabricationComponent } from './fabrication/fabrication.component';
+import { SingleProduitComponent } from './produit-list/single-produit/single-produit.component';
+import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitListComponent,
+    EspaceClientComponent,
+    MentionsLegalComponent,
+    PolitiqueConfComponent,
+    ContactComponent,
+    SocieteComponent,
+    FabricationComponent,
+    SingleProduitComponent,
+    NavbarHeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/politique-conf/politique-conf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-politique-conf',
+  templateUrl: './politique-conf.component.html',
+  styleUrls: ['./politique-conf.component.scss']
+})
+export class PolitiqueConfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
